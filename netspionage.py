7530244ba4bd4ea709a311781e79f4090e52a5ed
Angelina Tsuboi <angelina.t1832@gmail.com>
2023-01-14 10:33:52 -0800

    feat: update print_details

--- netspionage.py
+++ netspionage.py
@@ -2,38 +2,33 @@ import sys
 
 def print_banner():
        return ("""
-                  __             _                            
-      ____  ___  / /__________  (_)___  ____  ____ _____ ____ 
-     / __ \/ _ \/ __/ ___/ __ \/ / __ \/ __ \/ __ `/ __ `/ _ \\
-    / / / /  __/ /_(__  ) /_/ / / /_/ / / / / /_/ / /_/ /  __/
-   /_/ /_/\___/\__/____/ .___/_/\____/_/ /_/\__,_/\__, /\___/ 
-                      /_/                        /____/           
+                __             _                            
+    ____  ___  / /__________  (_)___  ____  ____ _____ ____ 
+   / __ \/ _ \/ __/ ___/ __ \/ / __ \/ __ \/ __ `/ __ `/ _ \\
+  / / / /  __/ /_(__  ) /_/ / / /_/ / / / / /_/ / /_/ /  __/
+ /_/ /_/\___/\__/____/ .___/_/\____/_/ /_/\__,_/\__, /\___/ 
+                    /_/                        /____/           
 """)
 
 def print_details():
-    return("   Created by Angelina Tsuboi [angelinatsuboi.net] V.0.0.1")
+    return("""
+ Created by Angelina Tsuboi [angelinatsuboi.net] V.0.0.1
+
+ https://github.com/angelina-tsuboi/netspionage
+ ----------------------------------------------------------------------------
+       """)
 
 
 def print_menu():
     return ("""
-ENTER 0 - 13 TO SELECT OPTIONS
+ENTER 0 - 5 TO SELECT OPTIONS
 
-1.  IP                           Enumerate  information  from  IP Address
-2.  DOMAIN                       Gather  information  about  given DOMAIN
-3.  PHONENUMBER                  Gather  information  about   Phonenumber
-4.  DNS MAP                      Map DNS  records associated  with target
-5.  METADATA                     Extract all metadata of  the  given file
-6.  REVERSE IMAGE SEARCH         Obtain domain name or IP address mapping
-7.  HONEYPOT                     Check if it's honeypot or a real  system
-8.  MAC ADDRESS LOOKUP           Obtain information about give Macaddress
-9.  IPHEATMAP                    Draw  out  heatmap  of  locations  of IP
-10. TORRENT                      Gather torrent download  history  of  IP
-11. USERNAME                     Extract Account info. from social  media
-12. IP2PROXY                     Check whether  IP  uses  any VPN / PROXY
-13. MAIL BREACH                  Checks given domain  has  breached  Mail
-99. UPDATE                       Update ReconSpider to its latest version
+1.  SCANNING                           Enumerate  information  from  IP Address
+2.  RECONNAISSANCE                       Gather  information  about  given DOMAIN
+3.  DETECTION                  Gather  information  about   Phonenumber
+4.  UPDATE                      Map DNS  records associated  with target
 
-0. EXIT                         Exit from  ReconSpider  to your terminal
+5. EXIT                         Exit from  ReconSpider  to your terminal
        """)
 
 if __name__ == '__main__':
@@ -41,6 +36,7 @@ if __name__ == '__main__':
            try:
                   print(print_banner())
                   print(print_details())
+                  print(print_menu())
                 #   from core import repl_prompt
            except ModuleNotFoundError:
                   print('\nSeems like you haven\'t installed Requirements or You are not using python3 version, Please install using: python3 setup.py install')
