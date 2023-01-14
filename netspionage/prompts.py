85df2141b77317a5ca6597da6dc0e3c74a8d07bd
Angelina Tsuboi <angelina.t1832@gmail.com>
2023-01-14 11:25:09 -0800

    feat: from netspionage import prompts

--- netspionage/prompts.py
+++ netspionage/prompts.py
@@ -1,8 +1,7 @@
 
 def prompt_display():  
     while 1:
-        print(menu())
-        user_input = prompt("\nReconspider >> ")
+        user_input = prompt("\nnetspionage >> ")
         if len(user_input)==0:
             print("\n")
             continue
@@ -16,112 +15,34 @@ def prompt_display():
             while 1:
                 ip = prompt("IP >> ")
                 break
-            shodan_host(ip)
-            censys_ip(ip)
+            # shodan_host(ip)
+            # censys_ip(ip)
             continue
 
-        elif choice == 2:
+        if choice == 2:
             while 1:
-                host = input("HOST (URL / IP) >> ")
-                port = input("PORT >> ")
-                try:
-                    if port == "":
-                        port=80
-                    elif int(port) not in [80,443]:
-                        print("Invalid port - Available(80,443)")
-                        continue
-                except ValueError:
-                    port=80
+                ip = prompt("IP >> ")
                 break
-            domain(host,int(port))
+            # shodan_host(ip)
+            # censys_ip(ip)
             continue
 
-        elif choice == 3:
+        if choice == 3:
             while 1:
-                ph = prompt("PHONE NUMBER (with CountryCode example) >> ")
+                ip = prompt("IP >> ")
                 break
-            Phonenumber(ph)
+            # shodan_host(ip)
+            # censys_ip(ip)
             continue
 
         elif choice == 4:
-            while 1:
-                dnsmap_inp = prompt("DNS MAP (URL) >> ")
-                break
-            dnsmap(dnsmap_inp)
-            continue
-
-        elif choice == 5:
-            while 1:
-                img_path = prompt("Metadata (PATH) >> ")
-                break
-            gps_analyzer(img_path)
-            continue
-
-        elif choice == 6:
-            while 1:
-                img = prompt("REVERSE IMAGE SEARCH (PATH) >> ")
-                break
-            reverseimagesearch(img)
-            continue
-
-        elif choice == 7:
-            while 1:
-                hp_inp = prompt("HONEYPOT (IP) >> ")
-                break
-            honeypot(hp_inp)
-            continue
-
-        elif choice == 8:
-            while 1:
-                mac = prompt("MAC ADDRESS LOOKUP (Eg:08:00:69:02:01:FC) >> ")
-                break
-            MacAddressLookup(mac)
-            continue
-
-        elif choice == 9:
-            while 1:
-                IPHeatmap()
-                break
-            continue
-
-        elif choice == 10:
-            while 1:
-                IP = prompt("IPADDRESS (Eg:192.168.1.1) >> ")
-                break
-            torrent(IP)
-            continue
-
-        elif choice == 11:
-            while 1:
-                print("\n1.Facebook \n2.Twitter \n3.Instagram\n")
-                username = input("Username >> ")
-                choice = input("choice >> ")
-                break
-            user(choice,username)
-            continue
-
-        elif choice == 12:
-            while 1:
-                IP = prompt("IPADDRESS (Eg:192.168.1.1) >> ")
-                break
-            ip2Proxy(IP)
-            continue
-
-        elif choice == 13:
-            while 1:
-                web = prompt("DOMAIN (Eg:intercom.io) >> ")
-                break
-            maildb(web)
-            continue
-
-        elif choice == 99:
             while 1:
                 break
             update()
             continue
 
-        elif choice == 0:
-            exit('\nBye, See ya again..')
+        elif choice == 5:
+            exit('\nTill next time!')
 
         else:
             pass
@@ -129,4 +50,4 @@ def prompt_display():
 try:
     prompt_display()
 except KeyboardInterrupt:
-    quit('\nBye, See ya again..')
+    quit('\nTill next time!')
