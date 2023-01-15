1a5388ccebb1c95bd75c10e63e273aed56d58620
Angelina Tsuboi <angelina.t1832@gmail.com>
2023-01-15 00:44:53 -0800

    feat: init detection

---
+++ core/detection.py
@@ -0,0 +1,9 @@
+# def detect_choice(choice, target):
+#     if choice == '1':
+#         chooseMACAddress(target)
+#         return()
+#     elif choice == '2':
+#         inputMACAddress(target, manual_input)
+#         return()
+#     else:
+#         exit()
