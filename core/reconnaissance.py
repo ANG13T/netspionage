50c06202f74d87a686256c41a1db9f6d64baa30e
Angelina Tsuboi <angelina.t1832@gmail.com>
2023-01-20 23:41:29 -0800

    feat: init mac add lookup

--- core/reconnaissance.py
+++ core/reconnaissance.py
@@ -12,4 +12,19 @@ def chooseMACAddress(target):
     return()
 
 def inputMACAddress(target, manual_input):
+    AddressAPICall(manual_input)
     return()
+
+
+def AddressAPICall(address):
+    url = ("https://macvendors.co/api/" + address)
+    response=requests.get(url)
+    result=response.json()
+    if result["result"]:
+        final=result['result']
+        print("Vendor:" + final["company"])
+        print("Address:" + final["address"])
+        print("Country:" + final["country"])
+        print("")
+    else:
+        print("Error: Something Went Wrong")
